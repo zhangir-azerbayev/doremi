4215d032d9371b6d7c0ca39889cbbaa3a7e60769
zhangir-azerbayev <zazerbayev@gmail.com>
2023-07-17 04:37:24 -0600

    fixed download.py

--- data/owm-mix/download.py
+++ data/owm-mix/download.py
@@ -10,9 +10,10 @@ from datasets import load_dataset, DatasetDict
 import tensorflow as tf
 import sentencepiece as spm
 
-def tokenize(batch, sp_tokenizer, max_length):
+def tokenize(batch, sp_tokenizer, max_length, domain_id):
     # tokenize
     examples = {"input_ids": sp_tokenizer.encode_as_ids(batch['text'])}
+    examples["attention_mask"] = [[True for _ in x] for x in examples["input_ids"]]
 
     # Concatenate all texts.
     examples = {k: list(chain(*examples[k])) for k in examples.keys()}
@@ -26,6 +27,7 @@ def tokenize(batch, sp_tokenizer, max_length):
         k: [t[i : i + max_length] for i in range(0, total_length, max_length)]
         for k, t in examples.items()
     }
+    result["domain_id"] = [domain_id for _ in range(len(result['input_ids']))]
     return result
 
 def filter_arxiv(example):
@@ -69,9 +71,8 @@ def main(args):
             print("downloading proof-pile...")
             ds = load_dataset("hoskinson-center/proof-pile")
 
-            for subset in ("arxiv", "stack_exchange", "rest"):
+            for domain_id, subset in enumerate(("arxiv", "stack_exchange", "rest")):
                 print(f"creating, tokenizing, saving {subset}")
-                Path(subset).mkdir()
 
                 flter = filter_dict[subset]
 
@@ -80,12 +81,14 @@ def main(args):
                 if max_rows:
                     ds_subset = trim_dataset(ds_subset, max_rows)
                 ds_toks = ds_subset.map(
-                        partial(tokenize, sp_tokenizer=sp, max_length=args.max_length),
+                        partial(tokenize, sp_tokenizer=sp, max_length=args.max_length, domain_id=domain_id),
                         batched=True,
                         remove_columns=['text', 'meta'],
                         num_proc=args.num_proc,
                 )
-                ds_toks.save_to_disk(subset)
+ 
+                for split in ["train", "test", "validation"]:
+                    ds_toks[split].save_to_disk(f"{split}/{subset}")
 
         elif ds_name=='open-web-math':
             ds = load_dataset("keirp/open-web-math-dev")["train"]
@@ -102,13 +105,18 @@ def main(args):
                 ds= trim_dataset(ds, max_rows)
 
             ds_toks = ds.map(
-                    partial(tokenize, sp_tokenizer=sp, max_length=args.max_length),
+                    partial(tokenize, sp_tokenizer=sp, max_length=args.max_length, domain_id=3),
                     batched=True,
                     remove_columns=['url', 'text', 'metadata'],
                     num_proc=args.num_proc,
             )
 
-            ds_toks.save_to_disk("open-web-math")
+            
+            for split in ["train", "test", "validation"]:
+                ds_toks[split].save_to_disk(f"{split}/open-web-math")
+        else:
+            raise ValueError("invalid dataset name {ds_name}")
+
                 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
@@ -119,7 +127,7 @@ if __name__=="__main__":
     )
     parser.add_argument("--max_rows", type=int, default=None)
     parser.add_argument("--num_proc", type=int, default=4)
-    parser.add_argument("--max_length", type=int, default=1024)
+    parser.add_argument("--max_length", type=int, default=2048)
     parser.add_argument("--eval_ratio", type=float, default=0.01)
 
     args = parser.parse_args()
